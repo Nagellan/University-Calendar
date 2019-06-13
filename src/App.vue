1ab60b9c1906e082073677fcdd9c3fe534f14e3e
Nagellan <31626879+Nagellan@users.noreply.github.com>
2019-06-13 16:42:54 +0300

    Add & Configure fonts * 'Elektra' font was added and configured into css file;

--- src/App.vue
+++ src/App.vue
@@ -14,11 +14,14 @@ export default {
 };
 </script>
 
-<style>
+<style lang="scss">
+@import "./assets/css/font.css";
+
 * {
   margin: 0;
   padding: 0;
   border: 0;
   outline: 0;
+  font-family: 'Elektra';
 }
 </style>
