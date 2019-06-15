e95fcecda1b90fa3cb0b48d0a8e454b819c3da9b
Nagellan <31626879+Nagellan@users.noreply.github.com>
2019-06-15 17:38:34 +0300

    Integrate Vuex in days rendering * Now present das are given from Vuex global storage.

--- src/components/Main.vue
+++ src/components/Main.vue
@@ -2,9 +2,9 @@
   <main>
     <groups-title />
     <day
-      v-for= "day in weekSchedule"
-      :key="day.name" 
-      :dayTitle="day.name"
+      v-for="day in presentDaysStatus"
+      :key="day"
+      :dayName="day"
     />
   </main>
 </template>
@@ -23,169 +23,205 @@ Vue.component('groups-title', {
 Vue.component('day', {
   render () {
     return (
-      <div class="day"> { this.dayTitle } </div>
+      <table class="day">
+        <tr>
+          <td> { this.dayName } </td>
+        </tr>
+      </table>
     )
   },
   props: [
-    'dayTitle'
+    'dayName'
   ]
 });
 
 export default {
+  methods: {
+    getDayName(dayNum) {
+      if (dayNum == 0)
+        return "Monday";
+      else if (dayNum == 1)
+        return "Tuesday";
+      else if (dayNum == 2)
+        return "Wednesday";
+      else if (dayNum == 3)
+        return "Thursday";
+      else if (dayNum == 4)
+        return "Friday";
+      else if (dayNum == 5)
+        return "Saturday";
+      else if (dayNum == 6)
+        return "Sunday";
+      else 
+        return null;
+    }
+  },
+  computed: {
+    presentDaysStatus: function () {
+      let daysArray = [];
+
+      for (let dayNum in this.dayStatus)
+        if (this.dayStatus[dayNum])
+          daysArray.push(this.getDayName(dayNum));
+
+      return daysArray;
+    }
+  },
   data() {
     return {
-     weekSchedule: [
-      {
-        name: "Monday",
-        groups: [
-          {
-            name: "BS1-1",
-            timeSlots: [
-              {
-                startTime: "9:00",
-                endTime: "10:30",
-                event: {
-                  type: "Lecture",
-                  room: 108,
-                  name: "Discrete Math and Logic",
-                  organizer: "Nikolay Shilov"
+      dayStatus: this.$store.getters.getDayStatus,
+      weekSchedule: [
+        {
+          name: "Monday",
+          groups: [
+            {
+              name: "BS1-1",
+              timeSlots: [
+                {
+                  startTime: "9:00",
+                  endTime: "10:30",
+                  event: {
+                    type: "Lecture",
+                    room: 108,
+                    name: "Discrete Math and Logic",
+                    organizer: "Nikolay Shilov"
+                  }
+                },
+                {
+                  startTime: "10:35",
+                  endTime: "12:05",
+                  event: {
+                    type: "Tutorial",
+                    room: 107,
+                    name: "Philosophy",
+                    organizer: "Joseph Brown"
+                  }
+                },
+                {
+                  startTime: "14:10",
+                  endTime: "15:40",
+                  event: {
+                    type: "Lab",
+                    room: 314,
+                    name: "Discrete Math and Logic",
+                    organizer: "Rustam Gafarov"
+                  }
                 }
-              },
-              {
-                startTime: "10:35",
-                endTime: "12:05",
-                event: {
-                  type: "Tutorial",
-                  room: 107,
-                  name: "Philosophy",
-                  organizer: "Joseph Brown"
+              ]
+            },
+            {
+              name: "BS1-2",
+              timeSlots: [
+                {
+                  startTime: "9:00",
+                  endTime: "10:30",
+                  event: {
+                    type: "Lecture",
+                    room: 108,
+                    name: "Discrete Math and Logic",
+                    organizer: "Nikolay Shilov"
+                  }
+                },
+                {
+                  startTime: "10:35",
+                  endTime: "12:05",
+                  event: {
+                    type: "Tutorial",
+                    room: 107,
+                    name: "Philosophy",
+                    organizer: "Joseph Brown"
+                  }
+                },
+                {
+                  startTime: "15:45",
+                  endTime: "17:15",
+                  event: {
+                    type: "Lab",
+                    room: 316,
+                    name: "Discrete Math and Logic",
+                    organizer: "Alexey Karachev"
+                  }
                 }
-              },
-              {
-                startTime: "14:10",
-                endTime: "15:40",
-                event: {
-                  type: "Lab",
-                  room: 314,
-                  name: "Discrete Math and Logic",
-                  organizer: "Rustam Gafarov"
+              ]
+            }
+          ]
+        },
+        {
+          name: "Tuesday",
+          groups: [
+            {
+              name: "BS1-1",
+              timeSlots: [
+                {
+                  startTime: "9:00",
+                  endTime: "10:30",
+                  event: {
+                    type: "Lecture",
+                    room: 108,
+                    name: "Discrete Math and Logic",
+                    organizer: "Nikolay Shilov"
+                  }
+                },
+                {
+                  startTime: "10:35",
+                  endTime: "12:05",
+                  event: {
+                    type: "Tutorial",
+                    room: 107,
+                    name: "Philosophy",
+                    organizer: "Joseph Brown"
+                  }
+                },
+                {
+                  startTime: "14:10",
+                  endTime: "15:40",
+                  event: {
+                    type: "Lab",
+                    room: 314,
+                    name: "Discrete Math and Logic",
+                    organizer: "Rustam Gafarov"
+                  }
                 }
-              }
-            ]
-          },
-          {
-            name: "BS1-2",
-            timeSlots: [
-              {
-                startTime: "9:00",
-                endTime: "10:30",
-                event: {
-                  type: "Lecture",
-                  room: 108,
-                  name: "Discrete Math and Logic",
-                  organizer: "Nikolay Shilov"
+              ]
+            },
+            {
+              name: "BS1-2",
+              timeSlots: [
+                {
+                  startTime: "9:00",
+                  endTime: "10:30",
+                  event: {
+                    type: "Lecture",
+                    room: 108,
+                    name: "Discrete Math and Logic",
+                    organizer: "Nikolay Shilov"
+                  }
+                },
+                {
+                  startTime: "10:35",
+                  endTime: "12:05",
+                  event: {
+                    type: "Tutorial",
+                    room: 107,
+                    name: "Philosophy",
+                    organizer: "Joseph Brown"
+                  }
+                },
+                {
+                  startTime: "15:45",
+                  endTime: "17:15",
+                  event: {
+                    type: "Lab",
+                    room: 316,
+                    name: "Discrete Math and Logic",
+                    organizer: "Alexey Karachev"
+                  }
                 }
-              },
-              {
-                startTime: "10:35",
-                endTime: "12:05",
-                event: {
-                  type: "Tutorial",
-                  room: 107,
-                  name: "Philosophy",
-                  organizer: "Joseph Brown"
-                }
-              },
-              {
-                startTime: "15:45",
-                endTime: "17:15",
-                event: {
-                  type: "Lab",
-                  room: 316,
-                  name: "Discrete Math and Logic",
-                  organizer: "Alexey Karachev"
-                }
-              }
-            ]
-          }
-        ]
-      },
-      {
-        name: "Tuesday",
-        groups: [
-          {
-            name: "BS1-1",
-            timeSlots: [
-              {
-                startTime: "9:00",
-                endTime: "10:30",
-                event: {
-                  type: "Lecture",
-                  room: 108,
-                  name: "Discrete Math and Logic",
-                  organizer: "Nikolay Shilov"
-                }
-              },
-              {
-                startTime: "10:35",
-                endTime: "12:05",
-                event: {
-                  type: "Tutorial",
-                  room: 107,
-                  name: "Philosophy",
-                  organizer: "Joseph Brown"
-                }
-              },
-              {
-                startTime: "14:10",
-                endTime: "15:40",
-                event: {
-                  type: "Lab",
-                  room: 314,
-                  name: "Discrete Math and Logic",
-                  organizer: "Rustam Gafarov"
-                }
-              }
-            ]
-          },
-          {
-            name: "BS1-2",
-            timeSlots: [
-              {
-                startTime: "9:00",
-                endTime: "10:30",
-                event: {
-                  type: "Lecture",
-                  room: 108,
-                  name: "Discrete Math and Logic",
-                  organizer: "Nikolay Shilov"
-                }
-              },
-              {
-                startTime: "10:35",
-                endTime: "12:05",
-                event: {
-                  type: "Tutorial",
-                  room: 107,
-                  name: "Philosophy",
-                  organizer: "Joseph Brown"
-                }
-              },
-              {
-                startTime: "15:45",
-                endTime: "17:15",
-                event: {
-                  type: "Lab",
-                  room: 316,
-                  name: "Discrete Math and Logic",
-                  organizer: "Alexey Karachev"
-                }
-              }
-            ]
-          }
-        ]
-      }
-     ]
+              ]
+            }
+          ]
+        }
+      ]
     };
   }
 };
