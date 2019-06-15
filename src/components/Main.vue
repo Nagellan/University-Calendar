0ed72f3dbe6203484a637ddb5a3693b7a7c894e1
Nagellan <31626879+Nagellan@users.noreply.github.com>
2019-06-15 12:14:18 +0300

    Render day components * They are empty yet; + Extend sample data.

--- src/components/Main.vue
+++ src/components/Main.vue
@@ -1,7 +1,11 @@
 <template>
   <main>
     <groups-title />
-    <day />
+    <day
+      v-for= "day in weekSchedule"
+      :key="day.name" 
+      :dayTitle="day.name"
+    />
   </main>
 </template>
 
@@ -19,17 +23,95 @@ Vue.component('groups-title', {
 Vue.component('day', {
   render () {
     return (
-      <div class="day">Day</div>
+      <div class="day"> { this.dayTitle } </div>
     )
-  }
+  },
+  props: [
+    'dayTitle'
+  ]
 });
 
 export default {
   data() {
     return {
-     schedule: [
+     weekSchedule: [
       {
-        day: "Monday",
+        name: "Monday",
+        groups: [
+          {
+            name: "BS1-1",
+            timeSlots: [
+              {
+                startTime: "9:00",
+                endTime: "10:30",
+                event: {
+                  type: "Lecture",
+                  room: 108,
+                  name: "Discrete Math and Logic",
+                  organizer: "Nikolay Shilov"
+                }
+              },
+              {
+                startTime: "10:35",
+                endTime: "12:05",
+                event: {
+                  type: "Tutorial",
+                  room: 107,
+                  name: "Philosophy",
+                  organizer: "Joseph Brown"
+                }
+              },
+              {
+                startTime: "14:10",
+                endTime: "15:40",
+                event: {
+                  type: "Lab",
+                  room: 314,
+                  name: "Discrete Math and Logic",
+                  organizer: "Rustam Gafarov"
+                }
+              }
+            ]
+          },
+          {
+            name: "BS1-2",
+            timeSlots: [
+              {
+                startTime: "9:00",
+                endTime: "10:30",
+                event: {
+                  type: "Lecture",
+                  room: 108,
+                  name: "Discrete Math and Logic",
+                  organizer: "Nikolay Shilov"
+                }
+              },
+              {
+                startTime: "10:35",
+                endTime: "12:05",
+                event: {
+                  type: "Tutorial",
+                  room: 107,
+                  name: "Philosophy",
+                  organizer: "Joseph Brown"
+                }
+              },
+              {
+                startTime: "15:45",
+                endTime: "17:15",
+                event: {
+                  type: "Lab",
+                  room: 316,
+                  name: "Discrete Math and Logic",
+                  organizer: "Alexey Karachev"
+                }
+              }
+            ]
+          }
+        ]
+      },
+      {
+        name: "Tuesday",
         groups: [
           {
             name: "BS1-1",
