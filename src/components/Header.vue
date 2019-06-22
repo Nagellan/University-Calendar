78fa77c0b1d3582866cb6bc6037714a453959606
mari1647iv <51486276+mari1647iv@users.noreply.github.com>
2019-06-22 15:18:47 +0300

    Add images to navbar

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -3,26 +3,26 @@
 			
 			<div class="left-nav-bar">
 				<div class="menu-circle"></div>
-				<img class="menu" src="menu_icon.png" />
-				<img class="logo" src="D:\AcademicSchedule\NavBar\image\IU_logo_RGB_2.png" /> 
+				<img class="menu" src="image\menu-icon.png" />
+				<img class="logo" src="image\IU_logo_RGB_2.png" /> 
 			</div>
 			
 			<div class="right-nav-bar">
 				<div class="academic-schedule">
 					<p class="schedule-button-text">Academic Schedule</p>
-					<img src="D:\AcademicSchedule\NavBar\image\more_button.png">				
+					<img src="image\more_button.png">				
 				</div>
 				<div class="room-schedule">
 					<p class="schedule-button-text">Room Schedule</p>
-					<img class="up" src="D:\AcademicSchedule\NavBar\image\more_button.png">				
+					<img class="up" src="image\more_button.png">				
 				</div>
 				
 				<div class="buttons">
 					<div class="full-screen-circle"></div>
-					<img class="full-screen" src="D:\AcademicSchedule\NavBar\image\full_screen_button-2.jpg" />
+					<img class="full-screen" src="image\full_screen_button-2.jpg" />
 					<div class="profile"><a href="https://my.university.innopolis.ru/" target="_blank">
 						<div class="profile-circle"></div>
-						<img class="profile-image" src="D:\AcademicSchedule\NavBar\image\profile-icon.png"/>
+						<img class="profile-image" src="image\profile-icon.png"/>
 					</a></div>
 				</div>
 			</div>
@@ -36,3 +36,4 @@ export default {};
 <style lang="scss" scoped>
 @import "../assets/css/header-style.css";
 </style>
+
