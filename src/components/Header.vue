1ceee5a4cf9a44cd9d200de361de0ecb35b7c23a
mari1647iv <51486276+mari1647iv@users.noreply.github.com>
2019-06-25 14:36:00 +0300

    Change images location

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -3,26 +3,26 @@
 			
 			<div class="left-nav-bar">
 				<div class="menu-circle"></div>
-				<img class="menu" src="image\menu-icon.png" />
-				<img class="logo" src="image\IU_logo_RGB_2.png" /> 
+				<img class="menu" src="../assets/images/menu-icon.png" />
+				<img class="logo" src="../assets/images/IU_logo_RGB_2.png" /> 
 			</div>
 			
 			<div class="right-nav-bar">
 				<div class="academic-schedule">
 					<p class="schedule-button-text">Academic Schedule</p>
-					<img src="image\more_button.png">				
+					<img src="../assets/images/more_button.png">				
 				</div>
 				<div class="room-schedule">
 					<p class="schedule-button-text">Room Schedule</p>
-					<img class="up" src="image\more_button.png">				
+					<img class="up" src="../assets/images/more_button.png">				
 				</div>
 				
 				<div class="buttons">
 					<div class="full-screen-circle"></div>
-					<img class="full-screen" src="image\full_screen_button-2.jpg" />
+					<img class="full-screen" src="../assets/images/full_screen_button-2.jpg" />
 					<div class="profile"><a href="https://my.university.innopolis.ru/" target="_blank">
 						<div class="profile-circle"></div>
-						<img class="profile-image" src="image\profile-icon.png"/>
+						<img class="profile-image" src="../assets/images/profile-icon.png"/>
 					</a></div>
 				</div>
 			</div>
