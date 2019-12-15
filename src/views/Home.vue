5cbb4e98d9a967d1e5a0bf7ef95e327df041d9a9
Nagellan <31626879+Nagellan@users.noreply.github.com>
2019-12-15 22:26:19 +0300

    Add url params functionality for weekdays

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -25,24 +25,51 @@ export default {
     RoomMain,
   },
   beforeCreate() {
+    let url = new URL(window.location.href);
+
     this.$store.dispatch("setInitialValue");
+
     // set schedule status from cookies
     let scheduleStatus = Cookies.getCookie("scheduleStatus");
     if (scheduleStatus)
       this.$store.dispatch("setScheduleStatus", +scheduleStatus);
+
     // set active room from cookies
     let roomActive = Cookies.getCookie("roomActive");
     if (roomActive)
       this.$store.dispatch("setActiveRoom", +roomActive);
+    
     // set checked days
-    let daysStatuses = Cookies.getCookie("daysStatuses");
-    if (daysStatuses)
-      this.$store.dispatch("setDaysStatuses", JSON.parse(daysStatuses));
-    // set active floors
+    if (url.searchParams.get("week")) {
+      // from url parameters
+      let weekdays = url.searchParams.get("week");
+      let daysStatuses = [
+        { "name":"Monday",    "isActive":false },
+        { "name":"Tuesday",   "isActive":false },
+        { "name":"Wednesday", "isActive":false },
+        { "name":"Thursday",  "isActive":false },
+        { "name":"Friday",    "isActive":false },
+        { "name":"Saturday",  "isActive":false },
+        { "name":"Sunday",    "isActive":false }
+      ];
+      daysStatuses.forEach((day) => {
+        if (weekdays.includes(day.name.slice(0, 3)))
+          day.isActive = true;
+      });
+      this.$store.dispatch("setDaysStatuses", daysStatuses);
+    } else {
+      // from cookies, otherwise, if they're specified
+      let daysStatuses = Cookies.getCookie("daysStatuses");
+      if (daysStatuses)
+        this.$store.dispatch("setDaysStatuses", JSON.parse(daysStatuses));
+    }
+
+    // set active floors from cookies
     let floors = Cookies.getCookie("floors");
     if (floors)
       this.$store.dispatch("setFloors", JSON.parse(floors));
-    // set toolbar state
+
+    // set toolbar state from cookies
     let toolbarStatus = Cookies.getCookie("toolbarStatus");
     if (toolbarStatus !== undefined)
       this.$store.dispatch("setToolBarStatus", toolbarStatus == "true");
